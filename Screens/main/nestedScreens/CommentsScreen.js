6b511d654847ce458f9964dbf68a9e4c0aae707d
Oleksandr Zhovty <alekszhov@gmail.com>
2023-02-08 16:30:13 +0100

    Made changes to comments render

--- Screens/main/nestedScreens/CommentsScreen.js
+++ Screens/main/nestedScreens/CommentsScreen.js
@@ -21,41 +21,35 @@ const CommentsScreen = ({ navigation, route }) => {
 
   const getPostData = async () => {
     await onSnapshot(doc(db, "posts", postId),(doc)=>setCommentsArr(doc.data().comments || []))
-   
-    
-    
-
   }
 
   
   useEffect(() => {
-   
     if (route.params.imgUri !== null) { setPhotoUri(route.params.imgUri) };
-    getPostData();
-  
-    
+    getPostData(); 
   }, [route.params])
   
 
   const toMakePost = () => {
-    const commentDate = new Date;
+    if(commentInpValue){const commentDate = new Date;
     const reqDate = modifyDate(commentDate)
     const comment = { text:commentInpValue, date: reqDate, userId, avatURL };
-    // const comments = commentsArr.length > 1 ? [...commentsArr, comment] : [comment]
     setDoc(doc(db, "posts", postId), { comments:[...commentsArr, comment]},{merge:true})
   
-    setCommentInpValue(null);
+    setCommentInpValue(null);}
   }
 
   const renderComments = ({ item }) => {
 
     return (
-     
-        <View style={st.commentCont}>
-        <View style={st.commentAvtr}><Image style={{ width: "100%", height: "100%", borderRadius: 14}} resizeMode="cover" source ={{uri: item.avatURL}}></Image></View>
-      <View style={st.commentTextCont}>
+      <View
+        style={userId === item.userId ? st.commentContCurrUsr : st.commentCont}
+       
+      >
+        <View style={userId === item.userId ? st.commentAvtrCurrUsr : st.commentAvtr}><Image style={{ width: "100%", height: "100%", borderRadius: 14}} resizeMode="cover" source ={{uri: item.avatURL}}></Image></View>
+      <View style={userId === item.userId ? st.commentTextContCurrUser:st.commentTextCont}>
             <Text style={st.commentText}>{item.text}</Text>
-            <Text style={st.commentTime}>{item.date}</Text>
+            <Text style={userId === item.userId ? st.commentTimeCurrUsr : st.commentTime}>{item.date}</Text>
       </View>
     </View>
    
@@ -65,6 +59,7 @@ const CommentsScreen = ({ navigation, route }) => {
   
   return <View style={st.cont}><Image style={st.postPhoto} source={{ uri: photoUri }} />
     <FlatList
+      style={{marginBottom:30}}
       data={commentsArr}
       renderItem={renderComments}
       keyExtractor={(item, idx) => idx.toString()} />
@@ -91,16 +86,26 @@ const st = StyleSheet.create({
     paddingTop:32,
     backgroundColor: "#fff",
     paddingHorizontal: 16,
-    paddingBottom: 32,
+    paddingBottom: 50,
    
   },
-  postPhoto: { height: 240, width: "100%", borderRadius: 8 , marginBottom:32},
-  commentCont: { flexDirection: "row", justifyContent: "space-between", marginBottom:24 },
+  postPhoto: { height: 240, width: "100%", borderRadius: 8, marginBottom: 32 },
+  
+  commentCont: { flexDirection: "row", justifyContent: "space-between", marginBottom: 24 },
+  commentContCurrUsr: { flexDirection: "row-reverse", justifyContent: "space-between", marginBottom: 24 },
+  
   commentAvtr: {
-    height: 28, width: 28, backgroundColor: "grey", marginRight: 16, borderRadius:14},
+    height: 28, width: 28, backgroundColor: "grey", marginRight: 16, borderRadius: 14
+  },
+  commentAvtrCurrUsr:{height: 28, width: 28, backgroundColor: "grey", marginLeft: 16, borderRadius: 14},
+  
   commentTextCont: { padding: 16, backgroundColor: "rgba(0,0,0,0.03)", flexGrow: 1, borderTopRightRadius: 8, borderBottomRightRadius: 8 },
+  commentTextContCurrUser: { padding: 16, backgroundColor: "rgba(0,0,0,0.03)", flexGrow: 1, borderTopLeftRadius: 8, borderBottomLeftRadius: 8 },
+  
   commentText: { fontFamily: "Roboto-Regular", fontSize: 13, color: "#212121", marginBottom: 8, },
+
   commentTime: { fontFamily: "Roboto-Regular", fontSize: 10, color: "#bdbdbd", textAlign: "right" },
+  commentTimeCurrUsr:{fontFamily: "Roboto-Regular", fontSize: 10, color: "#bdbdbd", textAlign: "left" },
   commInpCont: { backgroundColor: "#f6f6f6", minHeight: 50, borderRadius: 100, borderWidth:1, flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingLeft:16,paddingRight:8, position: "absolute", bottom:16, marginHorizontal:16,width:"100%" },
   commInpBtn: { backgroundColor: "#FF6C00", width: 34, height: 34, borderRadius: 17, justifyContent: "center", alignItems: "center" },
   commInput:{fontFamily:"Roboto-Medium"}
